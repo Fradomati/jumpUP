72229fd25e7aa062d80f3a5b386ec81d840ca0ac
Borja Domínguez <borja.d.atienza@gmail.com>
2019-12-09 16:59:10 +0100

    Bucle Renderizado top, done

--- js/game.js
+++ js/game.js
@@ -1,48 +1,54 @@
 let game;
 let w = window.innerHeight;
 let h = window.innerHeight;
+let delta = 0;
+let last = 0;
 
 const Game = {
     canvas: undefined,
     ctx: undefined,
     width: undefined,
     height: undefined,
-    fps:60,
 
-    preStart: function(){
+
+    fps: 60,
+
+    start: function () {
         this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
         this.width = window.innerWidth;
         this.height = window.innerHeight;
         this.canvas.width = this.width;
         this.canvas.height = this.height;
-        
-        this.start()
-    },
 
-    start: function(){
         this.createGame()
-
-        this.interval = setInterval(() => {
-            this.clear()
-            this.drawAll()
-        }, 1000/this.fps)
-        
-
+        a()
     },
 
-    createGame: function(){
+
+
+    createGame: function () {
         this.background = new Background(this.ctx, this.width, this.height)
     },
 
-    drawAll: function(){
+    drawAll: function () {
         this.background.draw();
     },
 
-    clear: function(){
-        this.ctx.clearRect(0,0,this.width,this.height)
+    clear: function () {
+        this.ctx.clearRect(0, 0, this.width, this.height)
     }
 }
+
+function a(){
+    function renderizado() {
+        window.requestAnimationFrame(renderizado);
+        Game.clear()
+        Game.drawAll()
+    }
+    window.requestAnimationFrame(renderizado)
+}
+
 
 /*
 class Game {
@@ -53,10 +59,10 @@ class Game {
         this.height = 1000;
         this.start()
         //this.background = new Background(this.ctx, this.width, this.height)
-        
+
     }
 
-    
+
     start() {
         this.createGame()
 
