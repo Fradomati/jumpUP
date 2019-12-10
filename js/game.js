462bf5dbedea331179afa2f2f0a9c0f9abaa9f2e
Borja Domínguez <borja.d.atienza@gmail.com>
2019-12-10 20:58:53 +0100

    Moves & jump

--- js/game.js
+++ js/game.js
@@ -3,12 +3,17 @@ let w = window.innerHeight;
 let h = window.innerHeight;
 let delta = 0;
 let last = 0;
+let jumperMoves = []
+let jumperJump = []
+let goJump = false;
+let goDown = false;
 
 const Game = {
     canvas: undefined,
     ctx: undefined,
     width: undefined,
     height: undefined,
+    // moves: [],
 
 
     fps: 60,
@@ -39,17 +44,65 @@ const Game = {
 
     clear: function () {
         this.ctx.clearRect(0, 0, this.width, this.height)
-    }
+    },
+
+    move: function () {
+        this.jumper.move()
+    },
+
+    jump: function() {
+        this.jumper.jump()
+        this.jumper.down()
+    },
+
 }
 
-function a(){
+// Bucle de renderizado
+
+function a() {
     function renderizado() {
         window.requestAnimationFrame(renderizado);
         Game.clear()
         Game.drawAll()
+        Game.move()
+        Game.jump()
     }
     window.requestAnimationFrame(renderizado)
 }
+
+// Keyboards
+
+document.addEventListener("keydown", function (e) {
+    if (e.keyCode == 65 || e.keyCode == 68) { jumperMoves.push(e.keyCode) }
+    else if (e.keyCode == 32) { jumperJump.push(e.keyCode) }
+
+})
+
+document.addEventListener("keyup", function () {
+    jumperMoves = []
+    goJump = true
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
--- js/player.js
+++ js/player.js
@@ -1,5 +1,5 @@
-class Jumper{
-    constructor(ctx, width, height, canvasWidth, canvasHeight){
+class Jumper {
+    constructor(ctx, width, height, canvasWidth, canvasHeight) {
         this.ctx = ctx;
         this.width = width;
         this.height = height;
@@ -9,10 +9,66 @@ class Jumper{
 
         this.posX = canvasWidth / 2 - height / 2;
         this.posY = canvasHeight * 0.98 - width;
+
+        this.vel = 6
+        this.power = 1
     }
 
 
-    draw(){
+    draw() {
         this.ctx.drawImage(this.image, this.posX, this.posY, this.width, this.height)
+    }
+
+    move() {
+        // Movimientos laterales, uso una array para que tenga un pequeño Sprint.
+        for (let i = 0; i < 2; i++) {
+            // Limpio el array de jumperMoves para que no carge la ram
+            if (jumperMoves.length % 5 == 0) jumperMoves.splice(2, 2)
+
+            if (jumperMoves[i] === 65) {
+                this.posX -= this.vel
+            }
+            else if (jumperMoves[i] === 68) {
+                this.posX += this.vel
+            }
+        }
+
+    }
+
+    jump() {
+      if(goJump === true){
+        if(this.power <= maxJump(jumperJump) ){
+            this.posY -= this.vel * 5
+            this.posX += this.vel * 2.5 // REVISAR EL ÁNGULO
+            this.power++
+        } else if(this.power > maxJump(jumperJump)){
+            this.power = 1
+            jumperJump = []
+            goJump = false;
+            goDown = true;
+        }
+        
+      }
+    }
+
+    down() {
+        if(goDown == true){
+            if(this.posY != 862.36){
+                this.posY += this.vel * 2.5
+                this.posX += this.vel / 2
+            } else if (this.posY == 862.36) {
+                goDown = false;
+            }
+        }
+    }
+}
+
+// Función para potencia de salto
+
+function maxJump(a){
+    if(a.length >= 20){
+        return 20
+    } else {
+        return a.length
     }
 }
